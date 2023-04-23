6e040ac3002c7307e2329570f39bb796105e2270
Muhammad Kattom <muhammadkattom@gmail.com>
2023-04-22 21:06:09 -0500

    Update ML
    Changed from classifier to regressor. Adding subtypes and category to remove ambiguity when algorithm returns ambiguous types

--- Research/mkatt9/ML.py
+++ Research/mkatt9/ML.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import OneHotEncoder
 
 # Load the CSV file into a pandas DataFrame
@@ -7,7 +7,7 @@ data = pd.read_csv('202304012238291fea.csv')
 assert isinstance(data, pd.DataFrame)
 
 # Drop rows containing missing values in the relevant columns
-data = data.dropna(subset=['postal_code', 'type', 'rating', 'photos_count'])
+data = data.dropna(subset=['postal_code', 'type', 'subtypes', 'category', 'rating', 'photos_count'])
 
 # Reset the index of the data DataFrame
 data.reset_index(drop=True, inplace=True)
@@ -16,76 +16,63 @@ data.reset_index(drop=True, inplace=True)
 data['postal_code'] = pd.to_numeric(data['postal_code'], errors='coerce')
 data = data.dropna(subset=['postal_code'])
 
-# Create a function to categorize ratings into classes
-def categorize_rating(rating):
-    if rating <= 2.5:
-        return 'low'
-    elif 2.5 < rating <= 4:
-        return 'medium'
-    else:
-        return 'high'
-
-# Apply the categorize_rating function to the 'rating' column
-data['rating'] = data['rating'].apply(categorize_rating)
-
-assert data['type'].dtype == 'object'
-assert data['rating'].dtype == 'object'
-assert data['photos_count'].dtype == 'float64'
-assert data['postal_code'].dtype == 'float64'
-
-
-# One-hot encode the 'type' column
+# One-hot encode the 'type', 'subtypes', and 'category' columns
 encoder = OneHotEncoder()
-encoded_type = encoder.fit_transform(data['type'].values.reshape(-1, 1))
-assert encoded_type.shape == (len(data), len(encoder.categories_[0]))
+encoded_columns = encoder.fit_transform(data[['type', 'subtypes', 'category']])
+column_names = encoder.get_feature_names_out(['type', 'subtypes', 'category'])
 
-# Create a new DataFrame with postal_code, photo_count, and encoded type columns
-encoded_type_df = pd.DataFrame(encoded_type.toarray(), columns=encoder.get_feature_names_out(['type']), index=data.index)
-X = pd.concat([data['postal_code'].astype(int), data['photos_count'], encoded_type_df], axis=1)
+# Create a new DataFrame with postal_code, photo_count, and encoded type, subtypes, and category columns
+encoded_columns_df = pd.DataFrame(encoded_columns.toarray(), columns=column_names, index=data.index)
+X = pd.concat([data['postal_code'].astype(int), data['photos_count'], encoded_columns_df], axis=1)
 X.dropna(subset=['postal_code'], inplace=True)
 
 assert isinstance(X, pd.DataFrame)
-#print(X['postal_code'])
-#X.to_csv('X_dataframe.csv', index=False)
 assert X['postal_code'].dtype == 'int32'
 
 # Create a new DataFrame with just the rating column
 y = data['rating']
 assert isinstance(y, pd.Series)
-assert y.dtype == 'object'
+assert y.dtype == 'float64'
 
-# Create a KNN classifier with k=5
-knn = KNeighborsClassifier(n_neighbors=5)
+# Create a KNN regressor with k=5
+knn = KNeighborsRegressor(n_neighbors=5)
 
-# Fit the classifier to the data
+# Fit the regressor to the data
 knn.fit(X, y)
 
 def find_best_restaurant_type(postal_code):
     assert isinstance(postal_code, int)
-    
+
     if postal_code < 60001 or postal_code > 62999 or len(str(postal_code)) != 5:
         print(f"Invalid postal code: {postal_code}. Please enter a valid Illinois zip code with 5 digits.")
         return None
 
-    # Create a DataFrame with the given postal code, a dummy photo_count, and the encoded 'type' column filled with zeros
+    # Create a DataFrame with the given postal code, a dummy photo_count, and the encoded columns filled with zeros
     encoded_postal_code = pd.DataFrame({'postal_code': [postal_code], 'photos_count': [0]})
-    encoded_postal_code = pd.concat([encoded_postal_code, pd.DataFrame(columns=encoder.get_feature_names_out(['type']), index=[0]).fillna(0)], axis=1)
+    encoded_postal_code = pd.concat([encoded_postal_code, pd.DataFrame(columns=column_names, index=[0]).fillna(0)], axis=1)
 
     # Find the 10 nearest neighbors to the given postal code
     neighbors = knn.kneighbors([encoded_postal_code.values[0]])[1][0]
 
-    # Get the restaurant types for those neighbors
-    types = data.iloc[neighbors]['type']
+    # Get the restaurant types, subtypes, and categories for those neighbors
+    neighbor_info = data.iloc[neighbors][['type', 'subtypes', 'category']]
 
-    # Count the occurrences of each type
-    type_counts = types.value_counts()
+    # Count the occurrences of each combination
+    combination_counts = neighbor_info.value_counts()
 
-    # Return the type with the highest count
-    best_type = type_counts.index[0]
-    assert isinstance(best_type, str)
-    
-    print(f"Zip code {postal_code}: The best restaurant type to open is '{best_type}'.")
-    return best_type
+    # Return the combination with the highest count
+    best_combination = combination_counts.index[0]
+
+    # Check if any subtype matches with the best combination
+    types_and_categories = [best_combination[0], best_combination[2]]
+    subtype_matches = [subtype for subtype in best_combination[1] if subtype in types_and_categories]
+
+    # Print the best combination
+    if subtype_matches:
+        print(f"Zip code {postal_code}: The best restaurant type to open is '{best_combination[0]}' with subtypes '{', '.join(subtype_matches)}' and category '{best_combination[2]}'.")
+    else:
+        print(f"Zip code {postal_code}: The best restaurant type to open is '{best_combination[0]}' with category '{best_combination[2]}'.")
+    return best_combination
 
 # Example usage
 find_best_restaurant_type(3)
